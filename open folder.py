c287932bb319cf0389ee7be87c2e45f9d54bf5f0
Your Real Name <relebohilesekhonyana9@gmail.com>
2026-03-22 23:14:22 +0200

    continue

--- open folder.py
+++
@@ -1,9 +0,0 @@
-import os
-
-current_folder = os.getcwd()
-print("Current folder:", current_folder)
-
-files = os.listdir(current_folder)
-print("Files in the current folder:")
-for file in files:
-    print(file)
